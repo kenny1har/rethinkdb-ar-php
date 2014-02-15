4b16781f4e63148293de82435cc337c8887c31ed
root <root@vesta.flathub.com>
2014-02-15 03:32:15 -0500

    v1.0

--- RDB.php
+++ RDB.php
@@ -1,18 +1,23 @@
 <?php
 class RDB {
+	public static $rdbDatabase;
 	public static $conn;
 	public static $params;
 
+	public static function connect($host, $port, $database, $password) {
+		self::$rdbDatabase = $database;
+		self::$conn = r\connect($host, $port, $database, $password);
+	}
 	public static function initialize() {
 		if (!isset(static::$table))
 			static::$table = substr(get_called_class(), strrpos(get_called_class(), '\\')+1);
-		static::$params = array();
+		self::$params[static::$table] = array();
 		foreach (get_class_vars(get_called_class()) as $key => $value)
-			if (!in_array($key, array('conn', 'params', 'table')))
-				static::$params[$key] = $value;
+			if (!in_array($key, array('conn', 'params', 'table', 'rdbDatabase')))
+				self::$params[static::$table][$key] = $value;
 	}
 	public static function get($id) {
-		$arr = \r\table(static::$table)->get($id)->run(static::$conn)->toNative();
+		$arr = \r\table(static::$table)->get($id)->run(self::$conn)->toNative();
 		$obj = new Static();
 		foreach ($arr as $key => $value) {
 			$obj->{$key} = $value;
@@ -21,21 +26,24 @@ class RDB {
 	}
 	public function save() {
 		$paramsTemp = array();
-		foreach (static::$params as $key => $value) {
+		foreach (self::$params[static::$table] as $key => $value) {
 			$paramsTemp[$key] = $this->$key;
 		}
 
 		if (isset($this->id)) {
-			return \r\table(static::$table)->get($this->id)->update($paramsTemp)->run(static::$conn);
+			return \r\table(static::$table)->get($this->id)->update($paramsTemp)->run(self::$conn);
 		} else {
 			unset($paramsTemp['id']);
-			return \r\table(static::$table)->insert($paramsTemp)->run(static::$conn);
+			return \r\table(static::$table)->insert($paramsTemp)->run(self::$conn);
 		}
 	}
 	public function delete() {
 		if (!isset($this->id))
 			return;
-		return \r\table(static::$table)->get($this->id)->delete()->run(static::$conn);
+		return \r\table(static::$table)->get($this->id)->delete()->run(self::$conn);
+	}
+	public static function createTable() {
+		\r\db(self::$rdbDatabase)->tableCreate(static::$table)->run(self::$conn);
 	}
 	public static function table() {
 		return \r\table(static::$table);
